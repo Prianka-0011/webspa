2d7e9eabd9faae52dffabb8ed5a88297204a4dbd
pranka mondal <priankanew0011@gmail.com>
2022-04-17 23:56:04 +0600

    ssl done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'webspa';
-}
+export class AppComponent implements OnInit{
+  title = 'Datingwebspa';
+  constructor(private http:HttpClient){
+
+  }
+  users:any;
+  ngOnInit(): void {
+    
+this.getUser();
+    }
+    getUser()
+    {
+      this.http.get('https://localhost:44348/api/user').subscribe(response=>{
+        this.users=response;
+        console.log("poku",this.users)
+      }
+    
+      );
+    }
+  }
+
