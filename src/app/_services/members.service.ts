6a3730a49e45a83adf3754b1ab28e7453c6a5071
pranka mondal <priankanew0011@gmail.com>
2022-08-25 00:48:56 +0600

    gg

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 // const httpHeader={
@@ -13,15 +13,32 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
 baseUrl=environment.apiUrl;
+members:Member[]=[];
   constructor(private http:HttpClient) { }
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+'user');
+    if(this.members.length>0)
+    return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl+'user')
+    .pipe(
+      map(members=>{
+      this.members=members;
+      return members
+  })
+    );
   }
   getMember(userName:string){
+    const member=this.members.find(x=>x.userName===userName);
+    if (member !==undefined)return of(member);
     return this.http.get<Member>(this.baseUrl+'user/'+userName);
   }
   updateMember(member:Member)
   {
-    return this.http.put<Member>(this.baseUrl+'user',member);
+    return this.http.put<Member>(this.baseUrl+'user',member)
+    .pipe(
+      map(()=>{
+      const index=this.members.indexOf(member);
+      this.members[index]=member;
+  })
+    );
   }
 }
--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,7 +9,7 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.scss']
 })
 export class MemberListComponent implements OnInit {
-members:Member[];
+members$:Observable<Member[]>;
   constructor(private memberService:MembersService) { }
 
   ngOnInit(): void {
@@ -16,9 +17,7 @@ members:Member[];
   }
   loadMembers()
   {
-    this.memberService.getMembers().subscribe(members=>{
-      this.members=members;
-    })
+    this.members$=this.memberService.getMembers();
   }
 
 }
