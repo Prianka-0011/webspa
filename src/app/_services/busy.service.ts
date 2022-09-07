74f56671512fd9faa0782a693b4f54ee7606dc13
pranka mondal <priankanew0011@gmail.com>
2022-09-08 00:18:20 +0600

    mm

--- src/app/_services/busy.service.ts
+++ src/app/_services/busy.service.ts
@@ -12,7 +12,8 @@ busyRequestCount=0;
     this.spinerService.show(undefined,{
       type:'line-scale-party',
       bdColor:'rgba(255,255,255,0)',
-      color:'#333333'
+      color:'#333333',
+      
     })
   }
   idle()
