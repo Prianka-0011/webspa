bd5f4ae245ede1752e5c2e370ad759bc8a22933a
pranka mondal <priankanew0011@gmail.com>
2022-09-10 00:08:55 +0600

    ismain photo

--- DatingApp/Controllers/AccountController.cs
+++ DatingApp/Controllers/AccountController.cs
@@ -49,7 +49,7 @@ namespace DatingApp.Controllers
         [HttpPost("login")]
         public async Task <ActionResult<UserDto>>Login(LoginDto login)
         {
-            var user =await  _context.Users
+            var user =await  _context.Users.Include(c=>c.Photos)
                 .SingleOrDefaultAsync(c => c.UserName == login.UserName);
             if (user == null)
             {
@@ -67,7 +67,9 @@ namespace DatingApp.Controllers
             return new UserDto
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl=user.Photos.FirstOrDefault(c=>c.IsMain)?.Url
+                
             };
         }
         private async Task<bool> UserExist(string userName)
